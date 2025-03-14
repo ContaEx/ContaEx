8303d5e31c372b258be09d436bb33d42b714fbdb
ContaEx <contaexdgeti@gmail.com>
2025-03-14 11:32:41 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// JavaScript para animaciones
+document.querySelectorAll('.button').forEach(button => {
+    button.addEventListener('mouseenter', () => {
+        button.style.transform = 'scale(1.05)';
+    });
+    button.addEventListener('mouseleave', () => {
+        button.style.transform = 'scale(1)';
+    });
+});
