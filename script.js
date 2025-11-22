869755d5563c2c77a5fcdeb61cb13e32f5a6fbf1
ContaEx <contaexdgeti@gmail.com>
2025-11-21 21:06:28 -0600

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// JavaScript para animaciones
-document.querySelectorAll('.button').forEach(button => {
-    button.addEventListener('mouseenter', () => {
-        button.style.transform = 'scale(1.05)';
-    });
-    button.addEventListener('mouseleave', () => {
-        button.style.transform = 'scale(1)';
-    });
-});
